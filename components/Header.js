47369af49d2b6e8875ed23e399e9bd7b369af418
Mukhtar Amin <mukhtaaramiin@gmail.com>
2022-03-19 12:09:50 +0300

    Initial Commit

---
+++ components/Header.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import style from "../styles/Header.module.css";
+import { Center, Group } from "@mantine/core";
+import { motion } from "framer-motion";
+
+function Header() {
+  const [Clicked, setClicked] = useState(false);
+  const trasition = { type: "spring", duration: 0.3, bounce: 0.6 };
+  const hover = { scale: 1.8 };
+  const marginTop = "5rem";
+  return (
+    <Group
+      direction="column"
+      position="center"
+      style={{
+        marginTop: Clicked ? "3rem" : "13rem",
+      }}
+      className={style.groupContainer}
+    >
+      <motion.div
+        transition={trasition}
+        whileHover={hover}
+        className={style.links}
+        onClick={() => setClicked(false)}
+      >
+        <p>Home</p>
+      </motion.div>
+      <motion.div
+        transition={trasition}
+        whileHover={hover}
+        className={style.links}
+        onClick={() => setClicked(true)}
+      >
+        <p>About me</p>
+      </motion.div>
+      <motion.div
+        transition={trasition}
+        whileHover={hover}
+        className={style.links}
+        onClick={() => setClicked(true)}
+      >
+        <p>Projects</p>
+      </motion.div>
+      <motion.div
+        transition={trasition}
+        whileHover={hover}
+        className={style.links}
+        onClick={() => setClicked(true)}
+      >
+        <p>Contact</p>
+      </motion.div>
+    </Group>
+  );
+}
+
+export default Header;
---
+++ components/Layout.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Header from "./Header";
+import { MantineProvider, Container } from "@mantine/core";
+
+function Layout({ children }) {
+  return (
+    <MantineProvider
+      defaultProps={{
+        Container: {
+          sizes: {
+            xs: 540,
+            sm: 720,
+            md: 960,
+            lg: 1140,
+            xl: 1320,
+          },
+        },
+      }}
+    >
+      <Container size="md">
+        <Header />
+        {children}
+      </Container>
+    </MantineProvider>
+  );
+}
+
+export default Layout;
+
+//  Hey, I'm Mukhtar! 👋
